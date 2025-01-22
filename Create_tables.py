14335dd07fb1a44d083568c2e05195188e90e0e1
rfrankeltcsbell <bellmore+rfrankel@thecoderschool.com>
2025-01-22 21:27:45 +0000

    first commit

---
+++ Create_tables.py
@@ -0,0 +1,76 @@
+import sqlite3
+
+# Connect to SQLite database (or create it)
+conn = sqlite3.connect("grocery_store.db")
+cursor = conn.cursor()
+
+# Drop tables if they already exist for a clean slate
+cursor.execute("DROP TABLE IF EXISTS Supplier")
+cursor.execute ("DROP TABLE IF EXISTS Customers")
+cursor.execute ("DROP TABLE IF EXISTS Products")
+cursor.execute ("DROP TABLE IF EXISTS Orders")
+cursor.execute ("DROP TABLE IF EXISTS OrdersDetails")
+
+# Create tables for the grocery store database
+# 1. Products Table
+cursor.execute("""
+CREATE TABLE Products(               
+    ProductID INTEGER PRIMARY KEY AUTOINCREMENT,
+    ProductName TEXT NOT NULL,
+    Category TEXT,           
+    Price REAL NOT NULL,
+    StockQuantity INTEGER NOT NULL,           
+    SupplierID INTEGER,
+    FOREIGN KEY (SupplierID) REFERENCES Suppliers(SuppliersID)
+ )
+""")
+
+# 2. Customers Table
+cursor.execute("""
+    CREATE TABLE Customers(               
+    CustomerID INTEGER PRIMARY KEY AUTOINCREMENT,
+    FirstName TEXT NOT NULL,
+    LastName TEXT NOT NULL,
+    Email TEXT UNIQUE,
+    LoyaltyPoints INTEGER DEFAULT 0
+               
+)
+""")
+ # 3. Orders Tables
+cursor.execute("""
+CREATE TABLE Orders                   
+OrderID INTEGER PRIMARY KEY AUTOINCREMENT,
+CustomerID INTEGER,
+OrderDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+TotalAmount REAL NOT NULL,
+FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+)
+ """)
+
+ #4. OrderDetails Table
+cursor.execute(""""
+ CREATE TABLE OrderDetails               
+  OrderDetailID INTEGER PRIMARY KEY AUTOINTERCREMENT,              
+  OrderID INTEGER,             
+   ProductID INTEGER,
+  Quantity INTEGER NOT NULL,
+  Price REAL NOT NULL,
+    FOREGIN KEY (OrderID) REFERENCES Orders(OrderID),
+    FOREIGN KEY (ProductID) REFERENCES Products(ProductsID)
+)
+""")
+
+ #5. Suppliers Table
+cursor.execute("""
+CREATE TABLE Suppliers                
+ SupplierID INTEGER PRIMARY KEY AUTOINCREMENT,
+ SupplierName TEXT NOT NULL,
+    ContantInfo Text
+ )
+""")
+
+# Commit changes and close connection
+conn.commit()
+conn.close()
+
+print("Database schema updated successfully!")
